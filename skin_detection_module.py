30553d96a3fb2ea90c2a395592a2226786d6f4ac
Fernanda <fbp@ecomp.poli.br>
2023-11-15 16:17:22 -0300

    refact: improve  skin detection by adding hue variable

--- skin_detection_module.py
+++ skin_detection_module.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import math
 
 
 def skin_detection(input_image):
@@ -11,15 +12,13 @@ def skin_detection(input_image):
 
     norm_image = np.stack([B, G, R], axis=2)
 
-    R = input_image[:, :, 2].astype("float")
-    G = input_image[:, :, 1].astype("float")
-    B = input_image[:, :, 0].astype("float")
+    R = R.astype("float")
+    G = G.astype("float")
+    B = B.astype("float")
 
     RGB = R+G+B
-
     r = R / RGB
     g = G / RGB
-    b = B / RGB
 
     # calculando limetes superiores e inferiores da cor da pele
     f1 = -1.376 * (r ** 2) + (1.0743 * r) + 0.2
@@ -28,9 +27,26 @@ def skin_detection(input_image):
     # calculando variavel w(hite) para indicar pixel branco
     w = (((r - 0.33)**2) + ((g - 0.33) ** 2)) > 0.001
 
+    # para maior acuracia vamos adotar elementos HIS(Hue, Intensity and Saturation)
+    divisor = 0.5*((R-G)+(R-B))
+    dividendo = np.sqrt(((R-G)**2) + ((R-B)*(G-B)))
+
+    # Evita divisão por zero
+    divisor[divisor == 0] = 1e-10
+    dividendo[dividendo < 1e-10] = 1e-10
+
+    # Garante que o argumento de np.arccos esteja no intervalo [-1, 1]
+    teta_arg = np.clip((divisor/dividendo), -1, 1)
+
+    # Calcular arco cosseno
+    teta = np.arccos(teta_arg)
+    teta = np.degrees(teta)
+
+    H = np.where(B <= G, teta,  360 - teta)
+
     # identificando o que é pele
     skin_mask = np.where(
-        ((g < f1) & (g > f2) & (w > 0.001)), 1, 0)
+        ((g < f1) & (g > f2) & (w > 0.001) & (np.bitwise_or(H > 240, H <= 20))), 1, 0)
 
     # Converte a máscara de pele para uma máscara de bits.
     skin_mask = skin_mask.astype("uint8")
@@ -67,6 +83,7 @@ if __name__ == '__main__':
         skin_detected_image = skin_detection(resized_img)
 
         cv.imshow("imagem com pele detectada", skin_detected_image)
-        cv.waitKey(0)
+
+        cv.waitKey()
     except Exception as e:
         print("Erro: ", e)
