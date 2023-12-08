277f27f7e8cfcf4164f2e785fb0a8a6b1925ff79
Fernanda <fbp@ecomp.poli.br>
2023-12-08 15:54:32 -0300

    feat: add hair qunatization

--- hair_detection_module.py
+++ hair_detection_module.py
@@ -28,3 +28,26 @@ def hair_detection(input_image):
     gray_img = cv.cvtColor(masked_image, cv.COLOR_BGR2GRAY)
 
     return gray_img
+
+
+def hair_quantization(hair_mask, square_size=5, threshold=12):
+    # Inicializa a imagem quantizada de cabelo com zeros
+    quantized_hair = np.zeros_like(hair_mask, dtype=np.uint8)
+
+    # Loop pelos pixels da máscara de cabelo
+    for y in range(0, hair_mask.shape[0], square_size):
+        for x in range(0, hair_mask.shape[1], square_size):
+            # Obtém o bloco 5x5 da máscara de cabelo
+            block = hair_mask[y:y+square_size, x:x+square_size]
+
+            # Conta o número de pixels brancos (valor 1) no bloco
+            black_pixel_count = np.sum(block == 0)
+
+            # Verifica se é um bloco de cor de cabelo ou não
+            is_hair_block = black_pixel_count > threshold
+
+            # Preenche o bloco correspondente na imagem quantizada de cabelo
+            quantized_hair[y:y+square_size, x:x +
+                           square_size] = is_hair_block.astype("uint8") * 255
+
+    return quantized_hair
